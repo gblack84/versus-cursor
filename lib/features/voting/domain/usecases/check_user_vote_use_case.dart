c8121b15dfda825158c9afeb19314fb70f7a1567
이철곤 <g_black@versus.space>
2025-10-26 11:29:06 +0900

    fix(voting): Update vote.dart import paths in usecases
    - Update import path in check_user_vote_use_case.dart
    - Update import path in get_vote_history_use_case.dart
    - Fix missing import updates from previous dialog/ migration
    
    Fixes 9 compilation errors:
    - uri_does_not_exist (2)
    - non_type_as_type_argument (4)
    - undefined_method (2)
    - unchecked_use_of_nullable_value (2)
    
    Related to: d0eb0fbc (VersusBoxSizeData migration)

--- lib/features/voting/domain/usecases/check_user_vote_use_case.dart
+++ lib/features/voting/domain/usecases/check_user_vote_use_case.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
-import '../models/vote.dart';
+import '../models/dialog/vote.dart';
 import '../repositories/i_voting_repository.dart';
 import 'base/use_case.dart';
 import '/core/errors/failures.dart';
